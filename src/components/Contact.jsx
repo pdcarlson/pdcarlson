3d2443e07a27e647dd18fc5bec895a2c5c322346
Paul Carlson <pdcarlson06@gmail.com>
2025-07-26 00:37:38 -0400

    refactor(Contact): simplify contact section by removing LinkedIn and GitHub links

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,25 +7,14 @@ function Contact() {
       <div className="contact-content">
         <h3>// Get In Touch</h3>
         <p>
-          I'm always open to discussing new projects, creative ideas, or opportunities to be part of an ambitious team. Feel free to reach out to me via email or connect with me on LinkedIn.
+          I'm always open to discussing new projects, creative ideas, or opportunities to be part of an ambitious team. Feel free to reach out to me via email.
         </p>
-        <div className="contact-links">
-          <p>
-            <i className="fa-solid fa-envelope"></i> 
-            <strong>Email:</strong> 
-            <a href="mailto:pdcarlson06@gmail.com"> pdcarlson06@gmail.com</a>
-          </p>
-          <p>
-            <i className="fa-brands fa-linkedin"></i> 
-            <strong>LinkedIn:</strong> 
-            <a href="https://linkedin.com/in/paul-carlson-rpi" target="_blank" rel="noopener noreferrer"> linkedin.com/in/paul-carlson-rpi</a>
-          </p>
-           <p>
-            <i className="fa-brands fa-github"></i> 
-            <strong>GitHub:</strong> 
-            <a href="https://github.com/pdcarlson" target="_blank" rel="noopener noreferrer"> github.com/pdcarlson</a>
-          </p>
-        </div>
+        <a 
+          href="mailto:pdcarlson06@gmail.com" 
+          className="email-link"
+        >
+          pdcarlson06@gmail.com
+        </a>
       </div>
     </section>
   );
