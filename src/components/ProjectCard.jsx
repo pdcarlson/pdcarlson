f8b1b833a3486f67306d00fcbfae8f04be86170f
Paul Carlson <pdcarlson06@gmail.com>
2025-07-28 21:15:20 -0400

    add handling for tech stack to render items from DB array

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 const ProjectCard = ({project : 
-    { title, desc, techStack, liveLink, codeLink}} ) => {
+    { title, desc, tech, liveLink, codeLink}} ) => {
   return (
         <div className="project-card">
           <h3>{title}</h3>
           <p className="project-description">{desc}</p>
-          <ul className="project-tech-list"></ul>
+          <ul className="project-tech-list">
+            {tech.map((techItem, index) => (
+              <li key={index} className="project-tech-item">{techItem}</li>
+            ))}
+          </ul>
           <div className="project-card-footer">
             <a href={liveLink} className="project-link-button" target="_blank" rel="noopener noreferrer">
               View Live
